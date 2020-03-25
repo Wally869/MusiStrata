7875039eab2c9ef4c07989f4609d1c2bd279f374
WillB <william.bele@edhec.com>
2020-03-25 17:25:00 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="MidiStructurer",
+    version="0.0.1",
+    url="http://github.com/Wally869/MidiStructurer",
+    author="Wally869",
+    packages=["MidiStructurer"],
+    install_requires=["mido"],
+    zip_safe=False
+)

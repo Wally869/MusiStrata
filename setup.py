cbfa9cc8b902b839693c7d7c22b9b7034d3568a4
unknown <william.bele@edhec.com>
2022-03-18 19:03:16 +0100

    modify settings in setup.py to fix error on import Components submodule

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="1.0.0",
     url="http://github.com/Wally869/MusiStrata",
     author="Wally869",
-    packages=["MusiStrata"],
+    packages=["MusiStrata", "MusiStrata.Components"],
     install_requires=["mido", "soundcard"],
     zip_safe=False
 )
